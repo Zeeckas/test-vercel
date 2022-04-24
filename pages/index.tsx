c159b6e88afd2c568f5f7b27856a506e3128eb19
Zeeckas <azaaaadh@gmail.com>
2022-04-24 23:11:03 +0300

    initialize repo

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react'
+
+const buttonClassName = 'px-4 py-3 text-semibold'
+
+function Counter() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <button className={buttonClassName} onClick={() => setCount(count + 1)}>
+        INCREMENT
+      </button>
+      <pre>{count}</pre>
+      <button
+        className={buttonClassName}
+        onClick={() => setCount(count > 0 ? count - 1 : count)}
+      >
+        DECREMENT
+      </button>
+    </div>
+  )
+}
+const Home: NextPage = () => {
+  return (
+    <main className="grid gap-4 p-5">
+      <pre>Hello Hackers!!!!</pre>
+      <Counter />
+    </main>
+  )
+}
+
+export default Home
